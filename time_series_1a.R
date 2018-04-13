7a0bf715b0ff442e7aeb8c3a8b5223daf4111820
pmalik88 <pmalik88@gmail.com>
2018-04-13 23:18:34 +0530

    h

--- time_series_1a.R
+++ time_series_1a.R
@@ -1,1 +1,14 @@
-datesla c('201819-1','2018-20-1')
+
+# Dealing with dates
+dates1a = c('2018-19-1','2018-20-1')
+class(dates1a)
+?as.Date
+as.Date(dates1a)
+
+dates1b = as.Date(dates1a,format ='%Y-%d-%m')
+class(dates1b)
+dates1b
+
+dates2a=c('2018-04-09')
+dates2b = as.Date(dates2a,format ='%Y-%d-%m')
+print(dates2b,format =('%d-%m-%Y'))
